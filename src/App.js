1896c576fda99dce3d7df1dc451dadbc3fe224f5
sahasrakeshava <167961467+sahasrakeshava@users.noreply.github.com>
2025-01-07 22:35:40 +0530

    added loadin bar before rendering the home page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import "./App.css";
 import { Route, Routes, Navigate } from "react-router-dom";
 import CustomerRouters from "./Routers/CustomerRouters";
 import AdminRouters from "./Routers/AdminRouters";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import LoginPage from "./customer/pages/LoginPage";
 import RegisterPage from "./customer/pages/RegisterPage";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CircularWithValueLabel from "./MainLoading.jsx"
+import CircularProgress from "@mui/material/CircularProgress";
+
+function GradientCircularProgress() {
+  return (
+    <React.Fragment>
+      <svg width={0} height={0}>
+        <defs>
+          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress
+        sx={{ "svg circle": { stroke: "url(#my_gradient)" } }}
+      />
+    </React.Fragment>
+  );
+}
+
 function App() {
   const { auth } = useSelector((store) => store);
-  const dispatch = useDispatch();
   const role = auth?.user?.role; // Extract the role of the user
 
   const [loading, setLoading] = useState(true);
@@ -19,7 +37,9 @@ function App() {
     // Ping the backend when the app loads
     const fetchData = async () => {
       try {
-        const response = await axios.get("https://kaveri-tailors-backend.onrender.com"); // Adjust the URL to your backend endpoint
+        const response = await axios.get(
+          "https://kaveri-tailors-backend.onrender.com"
+        ); // Adjust the URL to your backend endpoint
         // You can dispatch actions here if needed
         console.log("Backend responded:", response.data);
       } catch (error) {
@@ -33,14 +53,14 @@ function App() {
   }, []);
 
   if (loading) {
-  // Display a loading spinner or placeholder while the backend is being pinged
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <CircularWithValueLabel />
-    </div>
-  );
-}
-
+    // Display a loading spinner or placeholder while the backend is being pinged
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <GradientCircularProgress />
+        <br />
+      </div>
+    );
+  }
 
   return (
     <div className="App">
